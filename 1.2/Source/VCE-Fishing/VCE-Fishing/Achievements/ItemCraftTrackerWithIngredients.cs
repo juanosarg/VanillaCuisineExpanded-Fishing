e8a394452f5bba890b0ba4ed2aea7bf9c8ffb548
juanosarg <31105173+juanosarg@users.noreply.github.com>
2020-11-16 09:20:16 +0100

    v2.07

--- 1.2/Source/VCE-Fishing/VCE-Fishing/Achievements/ItemCraftTrackerWithIngredients.cs
+++ 1.2/Source/VCE-Fishing/VCE-Fishing/Achievements/ItemCraftTrackerWithIngredients.cs
@@ -6,24 +6,24 @@ using RimWorld;
 
 namespace AchievementsExpanded
 {
-    public class ItemCraftTrackerWithIngredients : Tracker<Thing>
+    public class ItemCraftTrackerWithIngredientsFishing : Tracker<Thing>
     {
 
-        public override string Key => "ItemCraftTrackerWithIngredients";
+        public override string Key => "ItemCraftTrackerWithIngredientsFishing";
 
         public override MethodInfo MethodHook => AccessTools.Method(typeof(QuestManager), nameof(QuestManager.Notify_ThingsProduced));
-        public override MethodInfo PatchMethod => AccessTools.Method(typeof(QuestManager_Notify_ThingsProduced_Patch), nameof(QuestManager_Notify_ThingsProduced_Patch.CheckItemCraftedIngredients));
+        public override MethodInfo PatchMethod => AccessTools.Method(typeof(VanillaFishing_QuestManager_Notify_ThingsProduced_Patch), nameof(VanillaFishing_QuestManager_Notify_ThingsProduced_Patch.CheckItemCraftedIngredients));
         protected override string[] DebugText => new string[] { $"Def: {def?.defName ?? "None"}",
                                                                 $"MadeFrom: {madeFrom?.defName ?? "Any"}",
                                                                 $"Quality: {quality}",
                                                                 $"Count: {count}",
                                                                 $"Current: {triggeredCount}" };
 
-        public ItemCraftTrackerWithIngredients()
+        public ItemCraftTrackerWithIngredientsFishing()
         {
         }
         public override (float percent, string text) PercentComplete => count > 1 ? ((float)triggeredCount / count, $"{triggeredCount} / {count}") : base.PercentComplete;
-        public ItemCraftTrackerWithIngredients(ItemCraftTrackerWithIngredients reference) : base(reference)
+        public ItemCraftTrackerWithIngredientsFishing(ItemCraftTrackerWithIngredientsFishing reference) : base(reference)
         {
             def = reference.def;
             madeFrom = reference.madeFrom;
@@ -49,15 +49,21 @@ namespace AchievementsExpanded
         public override bool Trigger(Thing thing)
         {
             base.Trigger(thing);
-            if ((def is null || thing.def == def) && (madeFrom is null || madeFrom == thing.Stuff)
-                && (includeingredient is null || (thing.TryGetComp<CompIngredients>().ingredients.Contains(includeingredient)))
-                )
+            if ((def is null || thing.def == def) && (madeFrom is null || madeFrom == thing.Stuff))
             {
-                if (quality is null || (thing.TryGetQuality(out var qc) && qc >= quality))
-                {
-                    triggeredCount = triggeredCount + thing.stackCount;
 
+                CompIngredients comping = thing.TryGetComp<CompIngredients>();
+
+                if (includeingredient is null || (comping != null && comping.ingredients.Contains(includeingredient)))
+                {
+                    if (quality is null || (thing.TryGetQuality(out var qc) && qc >= quality))
+                    {
+                        triggeredCount = triggeredCount + thing.stackCount;
+
+                    }
                 }
+
+
             }
             return triggeredCount >= count;
         }
--- 1.2/Source/VCE-Fishing/VCE-Fishing/Harmony/QuestManager_Notify_ThingsProduced_Patch.cs
+++ 1.2/Source/VCE-Fishing/VCE-Fishing/Harmony/QuestManager_Notify_ThingsProduced_Patch.cs
@@ -9,21 +9,30 @@ using RimWorld.Planet;
 
 namespace AchievementsExpanded
 {
-    public class QuestManager_Notify_ThingsProduced_Patch
+    public class VanillaFishing_QuestManager_Notify_ThingsProduced_Patch
     {
 
         public static void CheckItemCraftedIngredients(Pawn worker, List<Thing> things)
         {
-            foreach (var card in AchievementPointManager.GetCards<ItemCraftTrackerWithIngredients>())
+            if (things != null && worker != null)
             {
-                foreach (Thing thing in things)
+                foreach (var card in AchievementPointManager.GetCards<ItemCraftTrackerWithIngredientsFishing>())
                 {
-                    if ((card.tracker as ItemCraftTrackerWithIngredients).Trigger(thing))
+
+                    foreach (Thing thing in things)
                     {
-                        card.UnlockCard();
+                        if (thing != null && card != null)
+                        {
+                            if ((card.tracker as ItemCraftTrackerWithIngredientsFishing).Trigger(thing))
+                            {
+                                card.UnlockCard();
+                            }
+                        }
+
                     }
                 }
             }
+
         }
     }
 }
